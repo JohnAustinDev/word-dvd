227ad1965bd0c7ae8cd42f006746d1e0593f812f
ortoasia@gmail.com <ortoasia@gmail.com@2b84457f-8d23-2fef-b9be-9a654f29ce15>
2012-10-17 08:38:50 +0000

    Added prefs which make debugging easier.

---
+++ trunk/defaults/preferences/debug.js
@@ -0,0 +1,7 @@
+pref("browser.dom.window.dump.enabled", true);
+pref("dom.report_all_js_exceptions", true);
+pref("extensions.logging.enabled", true);
+pref("javascript.options.showInConsole", true);
+pref("javascript.options.strict", true);
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
